44b20345d426830c58f387a79220b38d8e8c07a8
Ben Beekman <bbeekman@gmail.com>
2019-10-03 13:49:38 -0700

    fixing Travis checks

--- r-p-s.js
+++ r-p-s.js
@@ -3,38 +3,44 @@ import { checkResult } from './check-result.js';
 
 const guessButton = document.getElementById('guess-button');
 const selectedRadioButton = document.querySelector('input:checked');
-const summarySpan = document.getElementById('summary');
+const resetButton = document.getElementById('reset-button');
 
 const winsCount = document.getElementById('wins-count');
 const losesCount = document.getElementById('loses-count');
 const drawsCount = document.getElementById('draws-count');
 let guessesCount = document.getElementById('guesses-count');
-let result = document.getElementById('result');
+let resultDisplay = document.getElementById('result');
+let guesses = 0;
+let result = '';
+let wins = 0;
+let losses = 0;
+let draws = 0;
 
-let totalGuesses = 0;
+const hardReset = () => {
+    guesses = 0;
+    result = '';
+    wins = 0;
+    losses = 0;
+    draws = 0;
+};
 
 // define DOM utility function
-const updateSpans = () => {
-    result.textContent = result;
-    correctGuesses.textContent = correctGuesses;
-    totalGuessesDisplay.textContent = totalGuesses;
-    winsCount.textContent = winsCount;
-    losesCount.textContent = losesCount;
-    drawsCount.textContent = drawsCount;
-    guessesCount.textContent = guessesCount;
+const updateResults = () => {
+    resultDisplay.textContent = result;
+    guessesCount.textContent = guesses;
+    winsCount.textContent = wins;
+    losesCount.textContent = losses;
+    drawsCount.textContent = draws;
 };
 
-const roundResult = () => {
+const playRound = () => {
     guessesCount++;
     const userPick = selectedRadioButton.value;
-    console.log(selectedRadioButton);
-    totalGuesses++;
     checkResult(userPick, getRandomThrow());
-    updateSpans();
-    debugger;
+    updateResults();
 };
-
 
 
 // add event listeners
-guessButton.addEventListener('click', roundResult);
+guessButton.addEventListener('click', playRound);
+resetButton.addEventListener('click', hardReset);
